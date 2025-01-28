64e5f7b16fae78c19757d9bf5d66971e958a163f
TanishMoral11 <ishantmoral@gmail.com>
2025-01-29 03:18:45 +0530

    Gemini Enabled Now

---
+++ app/src/main/java/com/theayushyadav11/agrowapp/api/gemini/GeminiResponse.kt
@@ -0,0 +1,5 @@
+package com.theayushyadav11.agrowapp.api.gemini
+
+data class GeminiResponse(
+    val candidates: List<Map<String, Any>>? = null
+)
--- app/src/main/java/com/theayushyadav11/agrowapp/util/Constants.kt
+++ app/src/main/java/com/theayushyadav11/agrowapp/util/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
 
 
         const val TRANSLATOR_API_BASE_URL = "https://deep-translator-api.azurewebsites.net"
+        const val GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/"
     }
 }
