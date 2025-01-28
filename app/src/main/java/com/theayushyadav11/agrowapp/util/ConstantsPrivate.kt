aa727a86a41195921a2c02d0365484c7264d9467
TanishMoral11 <ishantmoral@gmail.com>
2025-01-29 03:26:41 +0530

    rm

--- app/src/main/java/com/theayushyadav11/agrowapp/util/ConstantsPrivate.kt
+++ app/src/main/java/com/theayushyadav11/agrowapp/util/ConstantsPrivate.kt
@@ -2,7 +2,7 @@ package com.theayushyadav11.agrowapp.util
 
 class ConstantsPrivate {
     companion object {
-        const val GEMINI_API_KEY = "AIzaSyDvzvzDJK5sSEIrDIELiKodsXbIfCSgHS0"
+        const val GEMINI_API_KEY = ""
 
     }
 }
